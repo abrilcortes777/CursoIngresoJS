c89355394bd27b05fefd35b5ad73ada656b4c29f
Abril Cortes <abrilcortes777@gmail.com>
2023-01-31 17:15:08 -0300

    Ejercicio 5

--- 1-EntradaSalida/jsEntradaSalida-05.js
+++ 1-EntradaSalida/jsEntradaSalida-05.js
@@ -1,8 +1,19 @@
 /*
 Debemos lograr tomar nombre y edad por ID y mostrarlos concatenados 
 ej.: "Usted se llama José y tiene 66 años" 	*/
+//Abril Cortés Div X
 function mostrar()
 {	
-	alert("ok");
+	//Declaro las variables
+	let nombre;
+
+	let edad;
+
+	//Asigno valor a las variables
+	nombre = document.getElementById("txtIdNombre").value;
+
+	edad = document.getElementById("txtIdEdad").value;
+
+	alert("Usted se llama " + nombre + " y tiene " + edad + " años.");
 }
 
