8cafb0a80cb96a9d9adb36d3cc2c7110fe7e7432
Abril Cortes <abrilcortes777@gmail.com>
2023-02-01 09:59:58 -0300

    Ejercicio 7

--- 1-EntradaSalida/jsEntradaSalida-07.js
+++ 1-EntradaSalida/jsEntradaSalida-07.js
@@ -5,21 +5,78 @@ mostrar el resulto por medio de "ALERT"
 ej.: "la Resta es 750"*/ 
 function sumar()
 {	
-	alert("ok");	
+	//Declarar las variables
+	let primerNumero;
+	let segundoNumero;
+	let resultado;
+
+	//Definir variables
+	primerNumero = document.getElementById("txtIdNumeroUno").value;
+	segundoNumero = document.getElementById("txtIdNumeroDos").value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+    
+	resultado = primerNumero + segundoNumero;
+
+	alert("La suma es: " + resultado);
 }
 
 function restar()
 {
-	
+	//Declarar las variables
+	let primerNumero;
+	let segundoNumero;
+	let resultado;
+
+	//Definir variables
+	primerNumero = document.getElementById("txtIdNumeroUno").value;
+	segundoNumero = document.getElementById("txtIdNumeroDos").value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+    
+	resultado = primerNumero - segundoNumero;
+
+	alert("La resta es: " + resultado);
 }
 
 function multiplicar()
 { 
-	
+	//Declarar las variables
+	let primerNumero;
+	let segundoNumero;
+	let resultado;
+
+	//Definir variables
+	primerNumero = document.getElementById("txtIdNumeroUno").value;
+	segundoNumero = document.getElementById("txtIdNumeroDos").value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+    
+	resultado = primerNumero * segundoNumero;
+
+	alert("El producto es: " + resultado);
 }
 
 function dividir()
 {
-	
+	//Declarar las variables
+	let primerNumero;
+	let segundoNumero;
+	let resultado;
+
+	//Definir variables
+	primerNumero = document.getElementById("txtIdNumeroUno").value;
+	segundoNumero = document.getElementById("txtIdNumeroDos").value;
+
+	primerNumero = parseInt(primerNumero);
+	segundoNumero = parseInt(segundoNumero);
+    
+	resultado = primerNumero / segundoNumero;
+
+	alert("El cociente es: " + resultado);
+
 }
 
