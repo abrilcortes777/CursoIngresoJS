bc8c07dd0d6c9d1db81ad9949f10878f84b3604e
Abril Cortes <abrilcortes777@gmail.com>
2023-02-01 11:31:51 -0300

    Ejercicio 10

--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -3,7 +3,21 @@ Debemos lograr tomar el importe por ID.
 Transformarlo a entero (parseInt), luego
 mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
+// Abril Cortés Div X
 function mostrarAumento()
 {
-	alert("ok");
+let importe;
+let descuento;
+let importeConDescuento;
+
+importe = document.getElementById("txtIdImporte").value;
+importe = parseFloat(importe);
+
+
+
+descuento = importe * 25 / 100 ;
+
+importeConDescuento = importe - descuento;
+
+document.getElementById("txtIdResultado").value = importeConDescuento
 }
