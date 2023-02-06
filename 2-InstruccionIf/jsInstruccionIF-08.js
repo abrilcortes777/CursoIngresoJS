9d742a4e158979eadbc354d106969ee7dc3817ac
Abril Cortes <abrilcortes777@gmail.com>
2023-02-06 20:34:51 -0300

    Ejercicio 8 if

--- 2-InstruccionIf/jsInstruccionIF-08.js
+++ 2-InstruccionIf/jsInstruccionIF-08.js
@@ -1,8 +1,16 @@
+//Abril Cortés Div X
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
-	
+let edad;
+let estadoCivil;
 
+edad = document.getElementById("txtIdEdad").value;
+estadoCivil = document.getElementById("estadoCivil").value;
+
+edad = parseInt(edad);
+
+if(edad >= 18 && estadoCivil == "Soltero" ) {
+	alert("Es soltero y no es menor");
+}
 
 }//FIN DE LA FUNCIÓN
