f0456512918367c1d7c807e15c3eba12bdae25a2
Abril Cortes <abrilcortes777@gmail.com>
2023-02-06 19:55:55 -0300

    Ejercicio 6 if

--- 2-InstruccionIf/jsInstruccionIF-06.js
+++ 2-InstruccionIf/jsInstruccionIF-06.js
@@ -1,8 +1,22 @@
+//Abril Cortés Div X
 function mostrar()
 {
-	//tomo la edad  
-	alert("ok");
+	let edad;
+	edad = document.getElementById("txtIdEdad").value;
 
+	edad = parseInt(edad);
 
+	if (edad >= 18){
+	   alert("Es adolescente");
+	}
+    else{
+
+		if(edad >= 13){
+			alert("Es adolescente");
+		}
+		else{
+			alert("Es niño");
+		}
+	}
 
 }//FIN DE LA FUNCIÓN
