11926a5a4eac64759eee573ee358929f067e60d3
Abril Cortes <abrilcortes777@gmail.com>
2023-02-07 11:35:44 -0300

    Ejercicio 10 if

--- 2-InstruccionIf/jsInstruccionIF-10.js
+++ 2-InstruccionIf/jsInstruccionIF-10.js
@@ -1,6 +1,22 @@
 function mostrar()
 {
+	//Abril Cortés Div X
 	//Genero el número RANDOM entre 1 y 10 
-	alert("ok");
+	let nota;
+	 
+	nota = Math.floor(Math.random() *(1 + 10 - 1) + 1)
+		
+	if (nota >= 9) {
+		alert("Su nota es " + nota + "\n EXCELENTE");
+	}
+	else{
+		if(nota > 4){
+			alert("Su nota es " + nota + "\n APROBÓ");
+		}
+		else{
+			alert("Su nota es " + nota + "\n Vamos, la proxima se puede");
+		}
+	}
+
 
 }//FIN DE LA FUNCIÓN
