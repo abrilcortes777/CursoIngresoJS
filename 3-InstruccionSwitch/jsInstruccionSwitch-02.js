952961d1747858c7944156687aafe55477327c50
Abril Cortes <abrilcortes777@gmail.com>
2023-02-13 17:05:51 -0300

    Ejercicios 1,2 y 3 switch

--- 3-InstruccionSwitch/jsInstruccionSwitch-02.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-02.js
@@ -1,10 +1,25 @@
 function mostrar()
 {
-	//tomo el mes
-	var mesDelAño =txtIdMes.value;
-	alert(mesDelAño);
+	//Abril Cortes Div X
+let mesDelAnio;
 
+mesDelAnio = document.getElementById("txtIdMes").value;
 
+switch(mesDelAnio)
+{
+case "Septiembre":
+case "Octubre":
+case "Noviembre":
+case "Diciembre":
+	alert("Ya pasamos el frio,ahora hace calor");
+break;
+case "Julio":
+case "Agosto":
+break;
+default:
+	alert("Abrigate que hace frio");
+break;
+}
 
 
 }//FIN DE LA FUNCIÓN
